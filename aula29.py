d224ef6d73e97c4b2bf5d313110406d68f706f2c
Nuno Rocha <nuno.teles.rocha@gmail.com>
2023-04-26 23:48:19 +0100

    Novos ex

---
+++ aula29.py
@@ -0,0 +1,9 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erro ao tentar executar
+"""
+
+numero = float(input('Vou dobrar o numero que escolher: '))
+
+print(f'O dobro de {numero:.2f} é {numero * 2:.2f}') 
---
+++ aula31.py
@@ -0,0 +1,14 @@
+
+
+v1 = 'a'
+v2= 'a'
+
+print(id(v1))
+print(id(v2))
+
+condicao = False
+
+if condicao:
+    print('Faça alguma coisa.')
+else:
+    print('Não faça nada.')
